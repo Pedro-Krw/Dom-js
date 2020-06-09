11277c72a0a88c66d9920bf2a33f2c5693e07ed7
Pedro'Korwa <59316805+Pedro-korwa@users.noreply.github.com>
2020-06-09 14:04:50 +0700

    DOM-Berinteraksi dengan halaman HTML

--- seri Dom/Dom Tahapan 1/script.js
+++ seri Dom/Dom Tahapan 1/script.js
@@ -16,4 +16,17 @@ judul.innerHTML = 'Halaman Zoro';
 //ke-3 document.getElementsByClassName() -> mengembalikan HTML colection
   const pace = document.getElementsByClassName('p1');
 pace[0].innerHTML = 'ini di ubah mengunakan javascript';  
+  
+   dengan mengunakan 2 query  ini untuk supaya tidak merubah haaman htmlNya
+ke-4 document.querrySelctor() -> mengembalikan hanya 1 element
+ const p4 = document.querySelector('#b p');
+p4.style.color = 'white';
+p4.style.fontSize = '20px';
+p4.style.opacity = '80%';
+p4.style.cursor = 'pointer';
+p4.style.borderRadius = '20px';
+p4.style.backgroundColor = 'black';
 
+   //document.querySelectorAll() ->Mengembalikan semua elemant tapi megunakan indeks ke berapa
+  const p = document.querySelectorAll ('p');
+p[2].style.backgroundColor = 'pink';
