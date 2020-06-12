7d37189eea9914e76fa57fc32f0a189381e96c2b
pedrokorwa <korwapedro@gmail.com>
2020-06-12 13:29:40 +0700

    Upadate 13:29

--- seri Dom/Dom Tahapan 3/script.js
+++ seri Dom/Dom Tahapan 3/script.js
@@ -18,3 +18,27 @@ random.addEventListener("click", function () {
   const b = Math.round(Math.random() * 255 + 1);
   document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
 });
+const sMerah = document.querySelector("input[name=sMerah]");
+const sHijau = document.querySelector("input[name=sHijau]");
+const sBiru = document.querySelector("input[name=sBiru]");
+
+sMerah.addEventListener("input", function () {
+  const r = sMerah.value;
+  const g = sHijau.value;
+  const b = sBiru.value;
+  document.body.style.backgroundColor = "rgb(" + r + ", " + g + " , " + b + ")";
+});
+
+sHijau.addEventListener("input", function () {
+  const r = sMerah.value;
+  const g = sHijau.value;
+  const b = sBiru.value;
+  document.body.style.backgroundColor = "rgb(" + r + ", " + g + " , " + b + ")";
+});
+
+sBiru.addEventListener("input", function () {
+  const r = sMerah.value;
+  const g = sHijau.value;
+  const b = sBiru.value;
+  document.body.style.backgroundColor = "rgb(" + r + ", " + g + " , " + b + ")";
+});
